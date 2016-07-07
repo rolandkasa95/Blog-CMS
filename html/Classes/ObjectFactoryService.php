27cd426f8b6ed98657358cb14595bd6e800947e1
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-07 16:03:26 +0300

    New: ObjectFactoryService -- getConfig created

--- html/Classes/ObjectFactoryService.php
+++ html/Classes/ObjectFactoryService.php
@@ -2,5 +2,21 @@
 
 class ObjectFactoryService
 {
+    /**
+     * @var array
+     */
+    public static $config;
 
+
+    /**
+     * Database Configuration retrieval
+     *
+     * @return array|mixed
+     */
+    public static function getConfig(){
+        if (!self::$config) {
+            self::$config = require 'Config/config.php';
+        }
+        return self::$config;
+    }
 }
