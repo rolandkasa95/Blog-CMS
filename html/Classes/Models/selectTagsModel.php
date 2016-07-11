638d2e23c7e902c31bff9e21b0cbfa969e36f2bf
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-11 11:19:46 +0300

    New: selectTagsModel -> selectTags, connect

--- html/Classes/Models/selectTagsModel.php
+++ html/Classes/Models/selectTagsModel.php
@@ -11,5 +11,22 @@ namespace Models;
 
 class selectTagsModel
 {
+    protected $db;
 
+    public function connect($config){
+        try {
+            $this->db = new \PDO($config['dsn'], $config['user'], $config['pass']);
+        }catch (\PDOException $e){
+            echo "error: " . $e->getMessage();
+        }
+    }
+
+    public function selectTags(){
+        $config = \ObjectFactoryService::getConfig();
+        try{
+
+        }catch(\PDOException $e){
+            echo "Failed selection: " . $e->getMessage();
+        }
+    }
 }
