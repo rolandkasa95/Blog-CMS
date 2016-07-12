05a35360850a2d66197cdae99875f3c8679a46fa
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-12 17:12:59 +0300

    New: insertTagForm added __construct method

--- html/Classes/Forms/Inputs/insertTagForm.php
+++ html/Classes/Forms/Inputs/insertTagForm.php
@@ -1,15 +1,58 @@
 <?php
+
+namespace Forms;
+
+use Controllers\configController;
+use Models\selectTagsModel;
+use Validators\AlnumValidator;
+
 /**
- * Created by PhpStorm.
- * User: roland
- * Date: 12.07.2016
- * Time: 17:10
+ * Register Form Class
  */
-
-namespace Forms\Inputs;
-
-
-class insertTagForm
+class InsertArticleForm
+    extends FormCommon implements FormInterface
 {
+    /**
+     * RegisterForm constructor.
+     *
+     * This constructor sets the fields taken from
+     * FormBase to the RegisterForm output.
+     * @param $models
+     */
+    public function __construct($models)
+    {
+        $params = [
+            'name' => 'register',
+            'id' => 'form1',
+            'method' => 'post',
+            'action' => '#',
+        ];
+        parent::__construct($models, $params);
 
+        //Add username
+        $this->addField([
+            'label' => 'Tag Name: ',
+            'type' => 'text',
+            'name' => 'tag',
+            'priority' => 1,
+            'required' => true,
+            'value' => '',
+            'validator' => [
+                'StringLengthValidator' => [
+                    'minimum' => 2,
+                    'maximum' => 100,
+                ],
+                'AlfaValidator',
+                'DuplicationValidator',
+                'requiredValidator',
+            ],
+        ]);
+
+        //Adjust the button attributes
+        $button = $this->getField('Submit');
+        $button->setValue('Add Tag');
+
+        //Sort the fields by priority
+        ksort($this->fields);
+    }
 }
