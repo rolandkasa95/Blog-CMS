1e20fd9c9ac03b69d361c6ed4dd08450654e092f
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-18 13:23:02 +0300

    Update: DocBlocks for Models.2

--- html/Classes/Models/Session.php
+++ html/Classes/Models/Session.php
@@ -10,19 +10,34 @@ namespace Models;
  */
 class Session
 {
+    /**
+     * Session constructor.
+     */
     public function __construct()
     {
         session_start();
         $_SESSION['offset']=0;
     }
 
+    /**
+     * Session init.
+     */
     public function init(){
         $_SESSION['username'] = $_POST['username'];
     }
 
+    /**
+     * Returns the session variable
+     *
+     * @return array
+     */
     public function get(){
         return $_SESSION;
     }
+
+    /**
+     * Destroys the session
+     */
     public function destroy(){
         session_unset();
         session_destroy();
