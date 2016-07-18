b60998bd14b2c56a6815bbcb9f5c66918b804479
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-18 13:28:10 +0300

    Update: DocBlocks for Loader

--- html/Loader.php
+++ html/Loader.php
@@ -2,6 +2,9 @@
 
 class Loader
 {
+    /**
+     * Autoloader by namespaces
+     */
     public static function init(){
         spl_autoload_register(function ($class){
             $class = 'Classes/' . str_replace('\\', '/', $class) . '.php';
