d3d924b21b34304245e76109e60248e36ae9619d
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-19 14:35:31 +0300

    New: add a new Manage Tags Option

--- html/Layouts/html/adminPanel.php
+++ html/Layouts/html/adminPanel.php
@@ -17,5 +17,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <strong>If you want to add a new article: <br /><a href='index.php?action=insert'>Insert Article<a/></strong>
+            <strong>If you want to add a new article: <hr /><a href='index.php?action=insert'>Insert Article<a/></strong>
+            <strong>If you want to manage some tags: <hr /><a href="index.php?action=tags">Manage Tags</a></strong>
         </div>
+        <div class="col-md-6">
+            <?php
+                $this->model = new \Models\Model();
+                $result = $this->model->getArticles(0);
+                foreach($result as $row => $key)
+                {
+                if(isset($_SESSION['username'])) {
+                echo "<a href='index.php?action=edit&id=" . $this->model->getArticleId($row) . "'><div style='font-size: x-small; float: right; width: 30px;height: 30px; background-color: greenyellow'></div></a>";
+                }
+                echo "<a href=index.php?action=articleShow&id=" . $this->model->getArticleId($row) . " >" . $row . '</a><br />';
+                echo $key . '<br /><hr />';
+                }
+            ?>
+        </div>
+    </div>
+</div>
