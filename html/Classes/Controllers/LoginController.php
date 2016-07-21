b04e7fdc1fb8395347909295da461c26ab559149
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-21 16:01:25 +0300

    New: LoginController -> init()

--- html/Classes/Controllers/LoginController.php
+++ html/Classes/Controllers/LoginController.php
@@ -3,7 +3,26 @@
 namespace Controllers;
 
 
-class LoginController
-{
+use Views\View;
 
+class LoginController extends AppController
+{
+    public function init(){
+        $view = new View();
+        session_start();
+        if(!isset($_SESSION['username'])) {
+            $this->model = new Model();
+            if (session_start()) {
+
+                session_unset();
+                session_destroy();
+            }
+            $this->form = new LoginForm($this->model);
+            $view->render('loginpage.php', $this->form);
+        }else{
+            session_unset();
+            session_destroy();
+            $view->render('homePage.php',new Model());
+        }
+    }
 }
