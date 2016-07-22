ecf0c50b71b3a3dd9c2b1e71818bf78cca8eeb39
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-22 13:52:57 +0300

    New: FileInput -> construct + getInput

--- html/Classes/Forms/Inputs/FileInput.php
+++ html/Classes/Forms/Inputs/FileInput.php
@@ -12,4 +12,17 @@ namespace Forms\Inputs;
 class FileInput extends BaseInput
 {
 
+    public function __construct(){
+        $this->type = 'file';
+    }
+
+    /**
+     * @return string
+     */
+    public function getInput()
+    {
+        $required = $this->required ? ' required' : null;
+        return "<input type='$this->type' name='$this->name' value='$this->value'/>";
+    }
+
 }
