6fa90f2c6621a9b6aba152c9ca7db25e71ab6b0f
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-25 17:42:33 +0300

    Update: Manage Tags Controller bufix

--- html/Classes/Controllers/ManageTagsController.php
+++ html/Classes/Controllers/ManageTagsController.php
@@ -9,10 +9,33 @@ use Views\View;
 
 class ManageTagsController extends AppController
 {
-    public function init(){
+    public function init()
+    {
         $view = new View();
         $this->model = new manageTagsForm(new Model());
-        $view->render('manageTagsPage.php',$this->model);
+        $view->render('manageTagsPage.php', $this->model);
+        $this->manage();
     }
 
+    /**
+     * The form
+     *
+     * Validation and generation
+     */
+
+    public function manage()
+    {
+        ob_start();
+        header_remove();
+        $this->model = new \Models\Model();
+        var_dump(isset($_POST['submit']) && $_POST['submit'] === 'Delete');
+        if (isset($_POST['submit']) && $_POST['submit'] === 'Delete') {
+            $this->model->deleteTag($_SESSION['delete']);
+        }
+        var_dump(isset($_POST['submit']) && $_POST['submit'] === 'Update' && isset($_POST['updateTo']) && !empty($_POST['updateTo']));
+        if (isset($_POST['submit']) && $_POST['submit'] === 'Update' && isset($_POST['updateTo']) && !empty($_POST['updateTo'])) {
+            $this->model->updateTag($_SESSION['delete']);
+        }
+        ob_end_clean();
+    }
 }
