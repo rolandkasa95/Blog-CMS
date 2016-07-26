41b3471f333ee1ed175a97ae9c332bf61347f500
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-26 14:17:02 +0300

    New: Article Model -> setter+getters

--- html/Classes/Models/Article.php
+++ html/Classes/Models/Article.php
@@ -12,6 +12,87 @@ namespace Models;
 class Article extends Model
 {
     private $title;
-    private
+    private $id;
+    private $body;
+    private $urlImage;
 
+    public function __construct($param)
+    {
+        if(is_integer($param)){
+            $this->setId($param);
+        }elseif (is_string($param)){
+            $this->setTitle($param);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrlImage()
+    {
+        return $this->urlImage;
+    }
+
+    /**
+     * @param mixed $urlImage
+     */
+    public function setUrlImage($urlImage)
+    {
+        $this->urlImage = $urlImage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getById()
+    {
+        try{
+
+        }catch (\PDOException $e)
+    }
 }
