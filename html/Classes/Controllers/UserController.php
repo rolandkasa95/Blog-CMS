bc8c5e6e7338bfd7b138337595119c2ee68fc5a3
rolandkasa95 <rolandkasa95@gmail.com>
2016-08-02 15:09:54 +0300

    Update: userController: loginForm is secured

--- html/Classes/Controllers/UserController.php
+++ html/Classes/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 
+use Forms\LoginForm;
 use Models\Article;
 use Models\Articles;
 use Models\User;
@@ -14,18 +15,26 @@ class UserController
     
     public function init()
     {
+        $view = new View();
         if (isset($_GET['action'])){
             if('validate' === $_GET['action']){
                 if(isset($_POST['submit'])) {
                     $class = new User();
                     $class->setUsername($_POST['username']);
                     $username = $class->getUser();
-                    if (!empty($username)) {
+                    if ($username) {
                         session_start();
                         $_SESSION['username'] = $username['username'];
+                        header('Location: index.php');
                     };
-                    header('Location: index.php');
+                    $view->render('loginpage.php',new LoginForm(new User()));
+                    echo '<div id="login_div"> Please keep in mind: if you add new tags to the article, separate them with a comma! Thank you! </div>';
                 }
+            }
+        }
+        if (isset($_GET['action'])) {
+            if ('login' === $_GET['action']) {
+                $view->render('loginpage.php', new LoginForm(new User()));
             }
         }
     }
