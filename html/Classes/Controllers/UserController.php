bc14763666df79b643eeaf36a6c6cd9764241226
rolandkasa95 <rolandkasa95@gmail.com>
2016-08-11 19:56:27 +0300

    New: if function to tge app

--- html/Classes/Controllers/UserController.php
+++ html/Classes/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace Controllers;
 
 
 use Forms\LoginForm;
-use Models\Article;
 use Models\Articles;
 use Models\User;
 use Views\View;
@@ -16,6 +15,28 @@ class UserController
     public function init()
     {
         $view = new View();
+        if (isset($_GET['action'])) {
+            if (isset($_SESSION['username'])) {
+                if(PHP_SESSION_NONE === session_status() || PHP_SESSION_ACTIVE === session_status()){
+                    session_start();
+                    session_unset();
+                    session_destroy();
+                    $view->render('homePage.php', new Articles());
+                }else {
+                    $view->render('loginpage.php', new LoginForm(new User()));
+                }
+            }else{
+                if(PHP_SESSION_NONE === session_status() || PHP_SESSION_ACTIVE === session_status()){
+                    session_start();
+                    session_unset();
+                    session_destroy();
+                    $view->render('loginpage.php', new LoginForm(new User()));
+                }else {
+                    $view->render('loginpage.php', new LoginForm(new User()));
+                }
+            }
+        }
+
         if (isset($_GET['action'])){
             if('validate' === $_GET['action']){
                 if(isset($_POST['submit'])) {
@@ -30,16 +51,6 @@ class UserController
                     $view->render('loginpage.php',new LoginForm(new User()));
                     echo '<div id="login_div"> Please keep in mind: if you add new tags to the article, separate them with a comma! Thank you! </div>';
                 }
-            }
-        }
-        if (isset($_GET['action'])) {
-            if ('login' === $_GET['action']) {
-                if(PHP_SESSION_NONE === session_status() || PHP_SESSION_ACTIVE === session_status()){
-                    session_unset();
-                    session_destroy();
-                    $view->render('homePage.php', new Articles());
-                }
-                $view->render('loginpage.php', new LoginForm(new User()));
             }
         }
     }
